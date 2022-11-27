e3d264867bb5173da04fb4421472d359dbb50d14
kasm0005 <yahia.kasmi@etudiant.univ-reims.fr>
2022-11-27 01:12:52 +0100

    modifiying k

--- main.py
+++ main.py
@@ -6,7 +6,8 @@
 from tools import *
 
 if __name__ == '__main__':
-
+    importation('LoanData.csv')
+    preprocessing(loan)
     pca_tune(pipe, param_dict, X_train, y_train, X_test, y_test)
     knn_tune(pipe3, param_dict1, X_train, y_train, X_test, y_test)
     pipeline_logreg(X_train, y_train, X_test, y_test)
